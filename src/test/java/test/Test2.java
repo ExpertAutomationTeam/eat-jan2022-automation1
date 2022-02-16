12af471969b1e43d0db060b2439bc9b33fde4070
nacerhadjsaid <nacer.hadjsaid@gmail.com>
2022-02-16 14:25:07 -0500

    intro to automation & selecors

---
+++ src/test/java/test/Test2.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test2 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Users/nacer-zimu/IdeaProjects/eatjan172022automation1/src/driver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.com");
+    }
+
+    @Test
+    public void test1(){
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("computer");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+    }
+
+    @Test
+    public void test2(){
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("ps5");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+}
